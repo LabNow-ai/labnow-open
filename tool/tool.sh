57e449a444b398c030f15bba2da15f0920f829f9
haobibo <haobibo@noreploy.github.com>
2026-02-23 02:51:33 +0800

    ci debug

---
+++ tool/tool.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -xu
+
+CI_PROJECT_NAME=${CI_PROJECT_NAME:-$GITHUB_REPOSITORY}
+CI_PROJECT_BRANCH=${GITHUB_HEAD_REF:-"main"}
+CI_PROJECT_SPACE=$(echo "${CI_PROJECT_BRANCH}" | cut -f1 -d'/')
+
+if [ "${CI_PROJECT_BRANCH}" = "main" ] ; then
+    # If on the main branch, docker images namespace will be same as CI_PROJECT_NAME's name space
+    export CI_PROJECT_NAMESPACE="$(dirname ${CI_PROJECT_NAME})" ;
+else
+    # not main branch, docker namespace = {CI_PROJECT_NAME's name space} + "-" + {1st substr before / in CI_PROJECT_SPACE}
+    export CI_PROJECT_NAMESPACE="$(dirname ${CI_PROJECT_NAME})-${CI_PROJECT_SPACE}" ;
+fi
+
+export SRC_NAMESPACE="${REGISTRY_SRC:-docker.io}"
+export IMG_PREFIX_DST=$(echo "${REGISTRY_DST:-"docker.io"}/${CI_PROJECT_NAMESPACE}" | awk '{print tolower($0)}')
+export TAG_SUFFIX="-$(git rev-parse --short HEAD)"
+
+echo "--------> CI_PROJECT_NAMESPACE=${CI_PROJECT_NAMESPACE}"  # use different namespace for dev/prd
+echo "--------> DOCKER_SRC_NAMESPACE=${SRC_NAMESPACE}"
+echo "--------> DOCKER_IMG_PREFIX_DST=${IMG_PREFIX_DST}"
+echo "--------> DOCKER_TAG_SUFFIX=${TAG_SUFFIX}"
+
+[ ! -f /etc/docker/daemon.json ] && sudo tee /etc/docker/daemon.json > /dev/null <<< '{}'
+jq '.experimental=true | ."data-root"="/mnt/docker"' /etc/docker/daemon.json > /tmp/daemon.json && sudo mv /tmp/daemon.json /etc/docker/
+( sudo service docker restart || true ) && cat /etc/docker/daemon.json && docker info
+
+build_image() {
+    echo "$@" ;
+    IMG=$1; TAG=$2; FILE=$3; shift 3; VER=$(date +%Y.%m%d.%H%M)${TAG_SUFFIX}; WORKDIR="$(pwd)";
+    docker build --compress --force-rm=true -t "${IMG_PREFIX_DST}/${IMG}:${TAG}" -f "$FILE" --build-arg "BASE_NAMESPACE=${SRC_NAMESPACE}" "$@" "${WORKDIR}" ;
+    docker tag "${IMG_PREFIX_DST}/${IMG}:${TAG}" "${IMG_PREFIX_DST}/${IMG}:${VER}" ;
+}
+
+push_image() {
+    KEYWORD="${1:-second}";
+    docker image prune --force && docker images | sort;
+    IMAGES=$(docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.CreatedSince}}" | grep "${KEYWORD}" | awk '{print $1 ":" $2}') ;
+    echo "$DOCKER_REGISTRY_PASSWORD" | docker login "${REGISTRY_DST}" -u "$DOCKER_REGISTRY_USERNAME" --password-stdin ;
+    for IMG in $(echo "${IMAGES}" | tr " " "\n") ;
+    do
+      docker push "${IMG}";
+      status=$?;
+      echo "[${status}] Image pushed > ${IMG}";
+    done
+}
+
+clear_images() {
+    KEYWORD=${1:-'days ago\|weeks ago\|months ago\|years ago'}; # if no keyword is provided, clear all images build days ago
+    IMGS_1=$(docker images | grep "${KEYWORD}" | awk '{print $1 ":" $2}') ;
+    IMGS_2=$(docker images | grep "${KEYWORD}" | awk '{print $3}') ;
+
+    for IMG in $(echo "$IMGS_1 $IMGS_2" | tr " " "\n") ; do
+      docker rmi "${IMG}" || true; status=$?; echo "[${status}] image removed > ${IMG}";
+    done
+    docker image prune --force && docker images ;
+}
+
+
+remove_folder() {
+    sudo du -h -d1 "$1" || true ;
+    sudo rm -rf "$1" || true ;
+}
+
+free_diskspace() {
+    remove_folder /usr/share/dotnet
+    remove_folder /usr/local/lib/android
+    df -h
+}
